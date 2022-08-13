af682dabd23b75a13347a3815e0d9d4e9434090a
Amir <amir.h.rouh@gmail.com>
2022-08-13 19:13:29 +0430

    InitialTest1

--- Domain/Entities/Users'n'Roles/User.cs
+++ Domain/Entities/Users'n'Roles/User.cs
@@ -1,5 +1,6 @@
 ﻿using Domain.Entities.BaseEntities;
 using Domain.Entities.Orders;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,15 +9,20 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities.Users_n_Roles
 {
-    public class User : BaseEntityNoId
+    public class User : IdentityUser
     {
-        public long UserId { get; set; }
-        public string FullName { get; set; }
-        public string Email { get; set; }
-        public long? PhoneNumber { get; set; }
-        public string Password { get; set; }
-        public bool IsActive { get; set; } = true;
-        public ICollection<UserRoles> UserRoles { get; set; }
-        public ICollection<Order> Orders { get; set; }
+        //public long UserId { get; set; }
+        //public string FullName { get; set; }
+        //public string Email { get; set; }
+        //public long? PhoneNumber { get; set; }
+        //public string Password { get; set; }
+        //public bool IsActive { get; set; } = true;
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+        public DateTime? UpdateDate { get; set; }
+        public bool IsRemoved { get; set; } = false;
+        public DateTime? RemoveTime { get; set; }
+
+        public virtual ICollection<UserRoles> UserRoles { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
     }
 }
